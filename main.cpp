2ad17ebcb34e2a4be308dfbcb24eb19b79b859c6
ebadawy <ehalbadawy93@gmail.com>
2015-04-16 18:59:14 +0200

    Move I/O to a separated thread

--- main.cpp
+++ main.cpp
@@ -1,27 +1,57 @@
 #include <ncurses.h>
+#include<iostream>
+#include <pthread.h>
+#include <string>
+
+using namespace std;
+
+bool str_equals(char str[], string s);
+void *input(void*);
 
 int main(int argc, char *argv[]) {
-  WINDOW *input_win;
-  
+  pthread_t t1;
+ 
   initscr();
   cbreak();
 
-  refresh();
-  int max_x, max_y;
-
-  getmaxyx(stdscr, max_y, max_x);
-  input_win = newwin(1, max_x, max_y-1, 0);
-  
-  wprintw(input_win, ">>");
-  wrefresh(input_win);
-  char str[70];
-  wgetstr(input_win, str);
-  werase(input_win);
-  wprintw(input_win, ">>");
-  wrefresh(input_win);
-  printw("You Entered: %s", str);
-  refresh();
-  getch();
+  int iret1 = pthread_create(&t1, NULL, &input, NULL);
+  if(iret1)
+    printw("Error");
+  pthread_join(t1, NULL);
+    
   endwin();
   return 0;
 }
+
+
+bool str_equals(char str[], string s) {
+  for(int i = 0; i < s.length(); i++)
+    if(str[i] != s.at(i))
+      return false;
+  return !str[s.length()];
+}
+
+void *input(void*) {
+  WINDOW *input_win;
+  int max_x, max_y;
+
+  while(true){
+    getmaxyx(stdscr, max_y, max_x);
+    input_win = newwin(1, max_x, max_y-1, 0);
+
+    wprintw(input_win, ">>");
+    wrefresh(input_win);
+    char str[70];
+    wgetstr(input_win, str);
+    werase(input_win);
+    wprintw(input_win, ">>");
+    wrefresh(input_win);
+    if(!str_equals(str,"quit")) {
+      erase();
+      printw("You Entered: %s", str);
+      refresh();
+    } else
+      break; 
+  }
+}
+
