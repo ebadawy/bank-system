4819ad70a706ea4e570412f5fd22ad3e0f119227
ebadawy <ehalbadawy93@gmail.com>
2015-04-21 00:39:38 +0200

    Complete sys thread and print msgs in mlutiple lines

--- main.cpp
+++ main.cpp
@@ -16,14 +16,16 @@ void *input(void*);
 void *clock(void*);
 void *sys(void*);
 void print_scr(WINDOW *win, string str);
-
+void print_line(string str, int &row);
 Queue<Customer> wating_customers;
 LinkedList<Customer> serving_customers;
 Queue<Clerk> idel_clerks;
 LinkedList<Clerk> busy_clerks;
+int line = 0;
 
 time_t now;
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t print_line_mutex = PTHREAD_MUTEX_INITIALIZER;
 WINDOW *general_win;
 
 int main(int argc, char *argv[]) {
@@ -73,7 +75,7 @@ void *input(void*) {
     if(cmd.str() == "quit" && !str[4])
       break;
     else if(cmd.str() == "cust") {
-      int i = 8;
+      int i = 9;
       char c = str[i];
       name.str("");
       service.str("");
@@ -90,21 +92,25 @@ void *input(void*) {
       }
       if(service.str() != "withdraw" && service.str() != "depose" &&
          service.str() != "transfer" )
-        print_scr(general_win, "Malformed Service!");
+        print_line("Malformed Service!", line);
       else {
+        print_line(name.str() + " want to make a " + service.str() + " operation.", line);
         Customer c(name.str(), service.str(), now);
         wating_customers.enqueue(c);
       }
     } else if(cmd.str() == "cler") {
-      int i = 5;
+      int i = 6;
       char c = str[i];
       while(c) {
         name << c;
         c = str[++i];
       }
       Clerk k(name.str(), now);
+      print_line(name.str() + " has arrived.", line);
       idel_clerks.enqueue(k);
-    }
+    } else 
+      test("Can't resolve this command!", line, COLOR_GREEN); 
+    
   }
 }
 
@@ -135,17 +141,47 @@ void *sys(void*) {
     while(!wating_customers.is_empty() && !idel_clerks.is_empty()) {
       Customer c = wating_customers.dequeue();
       Clerk k = idel_clerks.dequeue();
-      c.set_clerk(k.get_name());
+      c.set_clerk(k);
       c.set_service_time(now);
-      serving_customers.enqueue(c);
-      k.set_customer(c.get_name());
-      busy_clerks.enqueue(k);
+      serving_customers.add(c);
+      k.set_customer(c);
+      busy_clerks.add(k);
       stm << k.get_name() << " is serving " << c.get_name();
-      print_scr(general_win, stm.str());
+      print_line(stm.str(), line);
+    }
+    Node<Customer>* current_customer_node = serving_customers.get_head();
+    string customer_name, clerk_name;
+    while(current_customer_node != NULL) {
+      if(current_customer_node->get_data().get_finish_time() <= now) {
+        print_line("finsihed", line);
+        Clerk *current_clerk = current_customer_node->get_data().get_clerk();
+        clerk_name = current_clerk->get_name();
+        customer_name = current_customer_node->get_data().get_name();
+        if(serving_customers.remove(current_customer_node->get_data()))
+          print_line(customer_name + " is now leaving.", line);
+        if(busy_clerks.remove(*current_clerk)) {
+          print_line(clerk_name + " is wating for another customer.", line);
+          idel_clerks.enqueue(*current_clerk);
+        }
+      }
+        current_customer_node = current_customer_node->get_next();  
     }
   }  
 }
-
+void print_line(string msg, int &row) {
+  pthread_mutex_lock( &print_line_mutex );
+  WINDOW *tmp;
+  int max_x, max_y;
+  getmaxyx(stdscr, max_y, max_x);
+  tmp = newwin(row+2, max_x, row+1, 0);
+  const char *c = msg.c_str();
+  pthread_mutex_lock( &mutex1 ); 
+  wprintw(tmp, c);
+  pthread_mutex_lock( &mutex1 );
+  wrefresh(tmp);
+  row++;
+  pthread_mutex_unlock( &print_line_mutex );
+}
 void print_scr(WINDOW *win, string msg) {
   pthread_mutex_lock( &mutex1 );
   const char *c = msg.c_str();
