5c7598fc043e9ac98dd756cd0e840675bf6b336f
ebadawy <ehalbadawy93@gmail.com>
2015-04-20 21:48:07 +0200

    Fix one-to-one relation between customer and clerk

--- Clerk.h
+++ Clerk.h
@@ -9,30 +9,29 @@ class Customer;
 class Clerk {
   private:
     std::string name;
-    std::string customer;
+    Customer* customer;
     time_t arrival_time;
   public:
     Clerk();
     Clerk(std::string n, time_t t);
     void set_name(std::string n) { name = n; }
-    void set_customer(std::string c) { customer = c; }
+    void set_customer(Customer c);
     void set_arrival_time(time_t t) { arrival_time = t; }
     time_t get_arrival_time() { return arrival_time; }
     std::string get_name() { return name; }
-    std::string get_cutomer() { return customer; }
+    Customer* get_cutomer() { return customer; }
+    Clerk& operator=(const Clerk& c);
     friend class Customer;
 };
 
 Clerk::Clerk() {
   name = "";
   arrival_time = -1;
-  customer = "";
 }
 
 Clerk::Clerk(std::string n, time_t t) {
   name = n;
   arrival_time = t;
-  customer = "";
 }
 
 #endif
--- Customer.h
+++ Customer.h
@@ -11,13 +11,13 @@ class Customer {
     time_t service_time;
     time_t finish_time;
     time_t arrival_time;
-    Clerk clerk;
+    Clerk* clerk;
   public:
     Customer();
     Customer(std::string n, std::string s, time_t arrival_t);
     void set_name(std::string n);
     void set_service(std::string s);
-    void set_clerk(Clerk c);
+    void set_clerk(Clerk c) { clerk = &c; } 
     void set_arrival_time(time_t t);
     void set_service_time(time_t t);
     std::string get_name() { return name; }
@@ -25,7 +25,7 @@ class Customer {
     time_t get_service_time() { return service_time; }
     time_t get_finish_time() { return finish_time; }
     time_t get_arrival_time() { return finish_time; }
-    Clerk get_clerk() { return clerk; }
+    Clerk* get_clerk() { return clerk; }
     Customer& operator=(const Customer& c);
     bool operator==(Customer &c) { return (name == c.get_name()); }
     friend class Clerk;
@@ -55,10 +55,6 @@ void Customer::set_service(std::string s) {
   service = s;
 }
 
-void Customer::set_clerk(Clerk c) {
-  clerk = c;
-}
-
 void Customer::set_arrival_time(time_t t) {
   arrival_time = t;
 }
@@ -71,6 +67,10 @@ void Customer::set_service_time(time_t t) {
     finish_time = service_time + 45;
   else if(service == "transfer")
     finish_time = service_time + 55;
+}
+
+void Clerk::set_customer(Customer c) {
+  customer = &c;
 }
 
 #endif
